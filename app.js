de8e686cb179218497b34f18c1b5189b8ab0d6ce
Rahul Garg <rahulgarg70100@gamil.com>
2022-09-24 17:23:30 +0530

    Commit after making the port active for live server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = (process.env.PORT || 5000);
 const request = require("request");
 const https = require("https");
 const bodyParser = require("body-parser");
